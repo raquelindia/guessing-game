387967b6cb1aa8756dd40bffb73502325eaf4ec8
raquelindia <raquelicruz94@gmail.com>
2023-04-12 14:09:54 -0400

    Add hint for numbers out of range

--- PlayRound.java
+++ PlayRound.java
@@ -6,6 +6,7 @@ public class PlayRound {
         int gamesWon = 0;
         int gamesLost = 0;
         int gamesPlayed = 0;
+       
         for(int j = 0; j < 3; j++){
         for(int i = 0; i < 5; i++){
                 int rounds = i + 1;
@@ -14,8 +15,40 @@ public class PlayRound {
 
                 UserGuess user = new UserGuess();
                 int userGuess = UserGuess.enterGuess();
-                //int userInt = Integer.parseInt(userGuess);
-
+                
+                
+                
+                
+                
+                if (userGuess > 10 || userGuess < 0){
+                    System.out.println("Hint: The number is 0 - 10" );
+                    /* 
+                    UserGuess user1 = new UserGuess();
+                    userGuess = UserGuess.enterGuess();
+                    rounds = rounds + 1;
+                    int newRounds = rounds - 1;
+                    if (rounds < 5 && computerGuess == userGuess){
+                        passOrFail = "WON";
+                        String message = "You " + passOrFail + "!";
+                        System.out.println(message + " Round: " + newRounds + " Games Played: " + gamesPlayed + " Scoreboard: You: " + gamesWon + " | Computer: " + gamesLost);
+                    } else if (rounds < 5 && computerGuess != userGuess) {
+                        passOrFail = "LOST";
+                        String messageTryAgain = "You " + passOrFail + "! Guess again...";
+                        System.out.println(messageTryAgain + " Round: " + newRounds);
+                    } else if (rounds == 5 && computerGuess == userGuess){
+                        gamesWon = gamesWon + 1;
+                        passOrFail = "WON";
+                        String gameOver2 = "You " + passOrFail + "!" + " GAME OVER...";
+                        System.out.println(gameOver2 + " Round: " + newRounds + " Games Played: " + gamesPlayed + " Scoreboard: You: " + gamesWon + " | Computer: " + gamesLost);
+                    } else if (rounds == 5 && computerGuess != userGuess){
+                        gamesLost = gamesLost + 1;
+                        passOrFail = "LOST";
+                        String gameOver = "You " + passOrFail + "!" + " GAME OVER...";
+                        System.out.println(gameOver + " Round: " + newRounds + " Games Played: " + gamesPlayed + " Scoreboard: You: " + gamesWon + " | Computer: " + gamesLost);
+                    } */
+                } 
+                
+           
             if (rounds == 5 && userGuess != computerGuess){
                 
                 passOrFail = "LOST";
@@ -47,12 +80,14 @@ public class PlayRound {
             }
         }
         if (gamesPlayed == 3 && gamesWon > gamesLost){
-            System.out.println("YOU BEAT THE COMPUTER! CONGRADULATIONS!! ~.~ <3 ^_^ <3");
+            System.out.println("YOU BEAT THE COMPUTER! CONGRATULATIONS!! ~.~ <3 ^_^ <3");
         } else if (gamesPlayed == 3 && gamesWon < gamesLost) {
+
             System.out.println("You let the computer beat you... train for 30 days and try again...");
         }
+        
+    
+        }
+    }
     }
 
-    
-    }
-}
